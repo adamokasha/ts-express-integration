d67feaef5f787e3d4c06e9767a4754dc9b330356
Adam Okasha <kasho.dev@gmail.com>
2019-11-10 17:28:23 -0500

    Add MetadataKeys enum

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export function controller(routePrefix: string) {
   // NOTE: if applied to a class the decorator target is constructor function (not the prototype)
@@ -10,10 +11,14 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
       // path will be decorated on our methods (through get decorator, for example)
-      const path = Reflect.getMetadata("path", target.prototype, key);
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
       // reflect returns any and router will not accept that so use Method enum which satisfies that interface
       const method: Methods = Reflect.getMetadata(
-        "method",
+        MetadataKeys.method,
         target.prototype,
         key
       );
