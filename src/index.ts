7cde86e2deaf7d1ede9a30378e54e377014c62ab
Adam Okasha <kasho.dev@gmail.com>
2019-11-10 12:39:33 -0500

    Add login routes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(
-    `
-    <div>
-    <h1> Hello World </h1>
-    </div>
-    `
-  );
-});
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</lable>
+        <input name="email"/>
+      </div>
+
+      <div>
+        <label>Password</lable>
+        <input name="password" type="password"/>
+      </div>
+        <button>Submit</button>
+    </form>  
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send({ email, password });
+});
+
+export { router };
